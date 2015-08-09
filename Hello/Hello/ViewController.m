1f948b115aa4ea1b1cbebb67c2e019402dc20b87
leo <1653012690@qq.com>
2015-08-09 21:57:38 +0800

    到

--- Hello/Hello/ViewController.m
+++ Hello/Hello/ViewController.m
@@ -19,6 +19,8 @@
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"hello world");
     NSLog(@"hahahaha");
+    NSLog(@"hahahaha");
+    NSLog(@"hahahaha");
 }
 
 - (void)didReceiveMemoryWarning {
